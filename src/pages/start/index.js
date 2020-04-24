3929eb0f61eae686e27ae0fc727609225b71ce2c
cx Luo <cxluo91@yahoo.com>
2020-04-24 18:20:34 +0800

    fix start

--- src/pages/start/index.js
+++ src/pages/start/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './index.less';
-
+import {NavLink} from 'react-router-dom';
 
 export default class Start extends React.Component{
     componentDidMount(){
@@ -13,18 +13,38 @@ export default class Start extends React.Component{
                <div className="indexStartOne">
                     <div className="indexStartOneWrapper content1200">
                         <div className="indexStartOneTextCons">
-                            <span className="indexStartOneTextConsChi">合作单位</span><br/>
-                            <span className="indexStartOneTextConsChi">负责审核合格的社区进入支持列表，审核申请者资格，并在活动期间为开发者、社区导师等参与方平台支持和沟通支持，为开发者、社区导师提供资金支持，项目开发周期结束后，主办方将进行成果汇总、奖金发放、证书颁发。</span>
+                            <span className="indexStartOneTextConsChi indexStartIcon indexStartIconOne"></span>
+                            <br/>
+                            <span className="indexStartOneTextConsChi"><h2>合作单位</h2></span>
+                            <br/>
+                            <span className="indexStartOneTextConsChi">和我们一起贡献开源！</span>
+                            <br/>
+                            <span className="indexStartOneTextConsChi"><h4><NavLink to="/contactus">联系我们</NavLink></h4></span>
+                            <br/>
+                            <span className="indexStartQuote">(请通过邮箱/公众号联系合作事宜)</span>
+
                         </div>
                         <div className="indexStartOneTextCons">
-                            
-                            <span className="indexStartOneTextConsChi">社区：</span><br/>
-                            <span className="indexStartOneTextConsChi">作为活动目标贡献的接收方，负责提供项目列表、项目指导、项目成果评审。</span>
+                            <span className="indexStartOneTextConsChi indexStartIcon indexStartIconTwo"></span>
+                            <br/>
+                            <span className="indexStartOneTextConsChi"><h2>社区</h2></span>
+                            <br/>
+                            <span className="indexStartOneTextConsChi">让更多的学生和开发者们加入你的社区!</span>
+                            <br/>
+                            <span className="indexStartOneTextConsChi"><h4><NavLink to="/contactus">立即报名</NavLink></h4></span>
+                            <br/>
+                            <span className="indexStartQuote">(请通过邮箱/公众号联系报名事宜)</span>
                         </div>
                         <div className="indexStartOneTextCons">
-                            
-                            <span className="indexStartOneTextConsChi">开发者：</span><br/>
-                            <span className="indexStartOneTextConsChi">自由选择项目，与社区导师沟通实现方案并撰写项目计划书。被选中的开发者将在社区导师指导下，按计划完成开发工作，并将成功贡献给社区。社区评估开发者的完成度，主办方根据评估结果发放资助金额给开发者的。</span>
+                            <span className="indexStartOneTextConsChi indexStartIcon indexStartIconThree"></span>
+                            <br/>
+                            <span className="indexStartOneTextConsChi"><h2>学生</h2></span>
+                            <br/>
+                            <span className="indexStartOneTextConsChi">利用暑期的时间，贡献你的代码吧！</span>
+                            <span className="indexStartOneTextConsChi"><h4>2020年6月1日，报名正式开始！</h4></span>
+                            <br/>
+                            <span className="indexStartQuote">(请通过邮箱/公众号联系报名事宜)</span>
+
                         </div>
                     </div>
                </div>
@@ -32,7 +52,7 @@ export default class Start extends React.Component{
                     <div className="indexStartOneWrapper content1200">
                         <div className="indexStartOneTextCons">
                             <span className="indexStartOneTextConsChi two">合作单位要求</span>
-                            <span className="indexStartOneTextConsChi">1. 国内具有法人的公司或机构。</span>
+                            <span className="indexStartOneTextConsChi">1. 支持开源的公司、高校、科研院所及其他机构。</span>
                         </div>
                         <div className="indexStartOneTextCons">
                             <span className="indexStartOneTextConsChi two">社区要求</span>
@@ -42,20 +62,11 @@ export default class Start extends React.Component{
                             <span className="indexStartOneTextConsChi">
                                 2. 开源软件仓库采用的协议需要是<a href="https://opensource.org/licenses">OSI 组织认可的协议</a>。 
                             </span>
-                            <span className="indexStartOneTextConsChi">
-                                3. 参与协议：需要补充社区参与的协议。 
-                            </span>
                         </div>
                         <div className="indexStartOneTextCons">
-                            <span className="indexStartOneTextConsChi two">开发者要求</span>
+                            <span className="indexStartOneTextConsChi two">学生要求</span>
                             <span className="indexStartOneTextConsChi">
-                                1. 在校学生或年满 18+ 周岁，提交学生证和身份证。
-                            </span>
-                            <span className="indexStartOneTextConsChi">
-                                2. 非在校学生仅提交身份证。
-                            </span>
-                            <span className="indexOneTextConsChi">
-                                参与协议：需要补充开发者参与的协议。
+                                1. 年满 18+ 周岁。提交学生证和身份证证明身份。
                             </span>
                         </div>
                     </div>
