b59c1b432d84ee5e944817de8c5867a6e96d8a45
cx Luo <cxluo91@yahoo.com>
2020-04-30 16:12:47 +0800

    add organisation

---
+++ src/pages/organisation/data.js
@@ -0,0 +1,52 @@
+const data = {
+    orgList:[
+        {
+            img:'ApacheAPISIX.jpg',
+            title:'ApacheAPISIX 社区',
+            stitle:'ApacheAPISIX'
+        },
+        {
+            img:'ApacheSkyWalking.jpg',
+            title:'ApacheSkyWalking 社区',
+            stitle:'ApacheSkyWalking'
+        },
+        {
+            img:'Linuxlab.jpg',
+            title:'Linuxlab',
+            stitle:'Linuxlab'
+        },
+        {
+            img:'MarkdownLab.jpg',
+            title:'Markdown Lab',
+            stitle:'MarkdownLab'
+        },
+        {
+            img:'MarkdownLab.jpg',
+            title:'Markdown Lab',
+            stitle:'MarkdownLab'
+        },
+        {
+            img:'RT-Thread.jpg',
+            title:'RT-Thread',
+            stitle:'RT-Thread',
+            url:'https://www.rt-thread.org/'
+        },
+        {
+            img:'RT-Thread.jpg',
+            title:'RT-Thread',
+            stitle:'RT-Thread'
+        },
+        {
+            img:'RT-Thread.jpg',
+            title:'RT-Thread',
+            stitle:'RT-Thread'
+        },
+        {
+            img:'RT-Thread.jpg',
+            title:'RT-Thread',
+            stitle:'RT-Thread'
+        },
+    ]
+}
+
+export default data
