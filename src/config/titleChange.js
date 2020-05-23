99d9875749bfc560aeadf070424d1f447f93ed10
xian <290455266@qq.com>
2020-05-23 22:20:21 +0800

    update title

--- src/config/titleChange.js
+++ src/config/titleChange.js
@@ -9,7 +9,7 @@ var gettitle = function(){
     // }else{
     //     document.title = `${linkDataMap[location[1]]||'首页'}${titleContent}`;
     // }
-    document.title = location[2] ? `${location[2]}社区详情-${titleContent}` : `${linkDataMap[location[1]]||'首页'}${titleContent}`
+    document.title = location[2] ? `社区详情${location[2]}-${titleContent}` : `${linkDataMap[location[1]]||'首页'}${titleContent}`
 }
 
 var titleChange = function(){
