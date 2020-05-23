572f7f9649a95e9493ece2bbe642549f2955d1a7
xian <290455266@qq.com>
2020-05-23 21:44:10 +0800

    update linkdata title

--- src/config/titleChange.js
+++ src/config/titleChange.js
@@ -4,19 +4,19 @@ import {readyWeixin} from './weixin.js';
 var gettitle = function(){
     var titleContent = ` - 开源软件供应链点亮计划 - 暑期2020 | 中国科学院软件研究所 | openEuler 社区`;
     var location = window.location.hash.split("?")[0].split("/");
-    if(location[2]){
-        document.title = location[2] +'社区详情-'+ titleContent;
-    }else{
-        document.title = `${linkDataMap[location[1]]||'首页'}${titleContent}`;
-    }
+    // if(location[2]){
+    //     document.title = location[2] +'社区详情-'+ titleContent;
+    // }else{
+    //     document.title = `${linkDataMap[location[1]]||'首页'}${titleContent}`;
+    // }
+    document.title = location[2] ? `${location[2]}社区详情-${titleContent}` : `${linkDataMap[location[1]]||'首页'}${titleContent}`
 }
 
 var titleChange = function(){
     gettitle()
     window.addEventListener('hashchange',()=>{
         window.scrollTo(0,0);
-        gettitle()
-        
+        gettitle() 
         readyWeixin();
     })
 
