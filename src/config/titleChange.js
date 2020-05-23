dd784d12221f9b6f7214f07a8ff9b718448d50ba
xian <290455266@qq.com>
2020-05-23 21:21:32 +0800

    update titlechange

--- src/config/titleChange.js
+++ src/config/titleChange.js
@@ -1,14 +1,22 @@
 import {linkDataMap} from './../components/header/listLink.js';
 import {readyWeixin} from './weixin.js';
 
-var titleChange = function(){
-    var hashname = linkDataMap[window.location.hash.split("#/")[1]]||'首页';
+var gettitle = function(){
     var titleContent = ` - 开源软件供应链点亮计划 - 暑期2020 | 中国科学院软件研究所 | openEuler 社区`;
-    document.title = hashname + titleContent;
-    window.addEventListener('hashchange',(old)=>{
+    var location = window.location.hash.split("?")[0].split("/");
+    if(location[2]){
+        document.title = location[2] +'社区详情-'+ titleContent;
+    }else{
+        document.title = `${linkDataMap[location[1]]||location[1]}${titleContent}`;
+    }
+}
+
+var titleChange = function(){
+    gettitle()
+    window.addEventListener('hashchange',()=>{
         window.scrollTo(0,0);
-        hashname = linkDataMap[old.newURL.split("#/")[1]] || '首页';
-        document.title = hashname + titleContent;
+        gettitle()
+        
         readyWeixin();
     })
 
