09c860583fbb6dce05f35515b97a60f76c249fd1
xian <290455266@qq.com>
2020-05-30 16:57:11 +0800

    add weixin test

--- src/config/weixin.js
+++ src/config/weixin.js
@@ -27,6 +27,7 @@ var readyWeixin = (title, description, imgUrl) => {
   if ((/mobile/.test(window.navigator.userAgent.toLowerCase())) ? true : false) {
     window.wx.ready(() => {
       //分享给朋友
+      console.log("weixin"+document.getElementsByTagName("meta")[2].content)
       window.wx.updateAppMessageShareData({
           title: title || document.title, // 分享标题
           desc: description || document.getElementsByTagName("meta")[2].content, // 分享描述
@@ -35,6 +36,7 @@ var readyWeixin = (title, description, imgUrl) => {
           success: function (res) {
             // 设置成功
             console.log(res)
+            
           }
       });
     
