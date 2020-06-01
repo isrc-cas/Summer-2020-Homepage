9c3c8e7ca55fd73830637628b1f85c473da3450e
cx Luo <cxluo91@yahoo.com>
2020-06-01 09:31:52 +0800

    add license comment

--- src/pages/start/index.js
+++ src/pages/start/index.js
@@ -1,7 +1,19 @@
+/**
+ * Copyright (c) 2020 Intelligent Software Research Center of ISCAS
+ * Summer 2020 Homepage is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
 import React from 'react'
 import './index.less';
 import './mobile-start.less';
-import data from './data.js'
+import data from './data.json'
 export default class Start extends React.Component{
     constructor(props){
         super(props)
