9bd855df87d4451216c695ee01cdee20e0290ddc
tracy <luochenxi@iscas.ac.cn>
2020-06-21 19:08:12 +0800

    update header and start banner

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -103,13 +103,7 @@ export default class Header extends React.Component{
                                             
                                             className={["osscListItem",this.state.pageflag===item.title?'left0':'',item.title==="started"?"header-highlight":""].join(" ")}>
                                         
-                                                <span> {item.name}
-                                                    {
-                                                    item.name === '我要申请' ?
-                                                    <img src={require("./../../img/fire.png")} alt="fire" style={{marginLeft: "3px", marginBottom: "15px"}}></img>
-                                                    : ''
-                                                    }
-                                                </span>
+                                                <span> {item.name} </span>
                                         
                                         
                                     </div>
