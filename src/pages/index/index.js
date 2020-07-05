ab380cb1cef40e2dd31bdfb79a34a7ff15605d36
xian <290455266@qq.com>
2020-07-05 17:31:33 +0800

    update liveurl

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -254,7 +254,7 @@ export default class Index extends React.Component{
                                 </div>
                                
                                 <div className="indexHomeOneListenItem fire"  onClick={()=>{this.goLink('liveshow')}}>
-                                    <span className="indexHomeOneListenItemText">7月4日  参与ASF开源贡献的正确姿势 - 如何成为一名合格的ASF Committer</span>
+                                    <span className="indexHomeOneListenItemText">7月18日  开源的商业创新</span>
                                     <span className="indexHomeOneListenItemTextButton">了解更多</span>
                                 </div>
                                 <div className="indexHomeOneListenItem" onClick={()=>{this.goLink('organisations?page=project')}}>
